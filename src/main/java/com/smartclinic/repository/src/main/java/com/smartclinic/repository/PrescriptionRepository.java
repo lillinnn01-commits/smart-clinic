86de64fb536dec27824c51d022554ef81990fcdf
lillinnn01-commits <lil.linnn01@gmail.com>
2026-08-03 22:59:39 +0500

    Create PrescriptionRepository.java

---
+++ src/main/java/com/smartclinic/repository/src/main/java/com/smartclinic/repository/PrescriptionRepository.java
@@ -0,0 +1,1 @@
+package com.smartclinic.repository; import com.smartclinic.model.Prescription; import org.springframework.data.jpa.repository.JpaRepository; public interface PrescriptionRepository extends JpaRepository<Prescription,Long>{}
